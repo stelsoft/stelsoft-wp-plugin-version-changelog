496ad56c1e0aa9e0456eca011af6c57a5dbf124c
Michał Stelmak <mstelmak@genymobile.com>
2018-06-29 15:03:29 +0200

    markdown

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ Version: 1.0
 Author URI: mailto:michal@stelmak.pl
 Author: Stelsoft Michał Stelmak
 */
+use Michelf\Markdown;
 
 class Stelsoft_Version_CHANGELOG {
 
@@ -24,12 +25,11 @@ class Stelsoft_Version_CHANGELOG {
             wp_die(__('You do not have sufficient permissions to access this page.'));
         }
 
+        include_once 'vendor/autoload.php';
+
         $str = file_exists(ABSPATH . 'CHANGELOG.md') ? file_get_contents(ABSPATH . 'CHANGELOG.md') : '<p>File not found.</p>';
-        $str = nl2br($str);
-//        $str = preg_replace('/^##\s(.*?)$/m', '<h3>$1</h3>', $str);
-//        $str = preg_replace('/^###\s(.*?)$/m', '<h4>$1</h4>', $str);
-//        $str = preg_replace('/^####\s(.*?)$/m', '<h5>$1</h5>', $str);
-        echo "<div><h2>CHANGELOG.md</h2><div>$str</div></div>";
+        $html = Markdown::defaultTransform($str);
+        echo "<div><h2>CHANGELOG.md</h2><div>$html</div></div>";
     }
 
 }
